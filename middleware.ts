17724b8a4dd2204661effa4342f0e1ac14ac85e7
rajulasgithub <rajulasrazak688@gmail.com>
2025-12-13 20:57:06 +0530

    middleware added

---
+++ middleware.ts
@@ -0,0 +1,87 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const publicRoutes = ["/login", "/register", "/about"];
+
+const adminRoutes = ["/admin"];
+
+const sellerRoutes = [
+    "/sellerdashboard",
+    "/addbook",
+    "/sellerprofile",
+    "/sellerorders",
+    "/sellerbooks",
+    "/editprofile",
+    "/sellersingleorder",
+    "/updatebook",
+];
+
+const customerRoutes = [
+    "/checkout",
+    "/customerprofile",
+    "/editprofile",
+    "/viewbooks",
+    "/viewonebook",
+    "/vieworders",
+    "/cart",
+    "/wishlist",
+];
+
+export function middleware(request: NextRequest) {
+    const { pathname } = request.nextUrl;
+
+    const token = request.cookies.get("accessToken")?.value;
+    const role = request.cookies.get("role")?.value;
+
+    
+   const referer = request.headers.get("referer");
+
+
+if (token && pathname === "/" && !referer) {
+  if (role === "seller") {
+    return NextResponse.redirect(
+      new URL("/sellerdashboard", request.url)
+    );
+  }
+
+  if (role === "customer") {
+    return NextResponse.redirect(
+      new URL("/viewbooks", request.url)
+    );
+  }
+
+  if (role === "admin") {
+    return NextResponse.redirect(
+      new URL("/admin", request.url)
+    );
+  }
+}
+
+
+
+    const isPublicRoute = publicRoutes.some(
+        (route) => pathname === route || pathname.startsWith(`${route}/`)
+    );
+
+    if (!token && !isPublicRoute) {
+        return NextResponse.redirect(new URL("/login", request.url));
+    }
+
+    if (adminRoutes.some((route) => pathname.startsWith(route)) && role !== "admin") {
+        return NextResponse.redirect(new URL("/unauthorized", request.url));
+    }
+
+    if (  sellerRoutes.some((route) => pathname.startsWith(route)) &&role !== "seller") {
+        return NextResponse.redirect(new URL("/unauthorized", request.url));
+    }
+
+    if ( customerRoutes.some((route) => pathname.startsWith(route)) && role !== "customer") {
+        return NextResponse.redirect(new URL("/unauthorized", request.url));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ["/((?!_next|favicon.ico|assets|api).*)"],
+};
