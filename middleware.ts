881492db00b9c873c20507f6373dd877d5181d57
rajulasgithub <rajulasrazak688@gmail.com>
2025-12-16 11:16:20 +0530

    middleware updated

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const publicRoutes = ["/login", "/register", "/about"];
+const publicRoutes = ["/", "/login", "/register", "/about"];
 
 const adminRoutes = ["/admin"];
 
